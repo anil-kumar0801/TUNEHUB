da583515639a8cc93cd0a4235e980ceb054311f6
anil-kumar0801 <9361anil@gmail.com>
2024-01-13 08:01:38 +0530

    1st version

---
+++ src/main/java/com/example/demo/controller/NavController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class NavController {
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	@GetMapping("/registeration")
+	public String registeration() {
+		return "registeration";
+	}
+	
+	@GetMapping("/newSong")
+	public String newSong() {
+		return "newSong";
+	}
+	
+}
